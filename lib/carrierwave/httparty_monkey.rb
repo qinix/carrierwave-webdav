1b72276d3f16af3b8e93648881e0de4d35ec911c
Duka ÃrpÃ¡d <duka@duka.duka.info.hu>
2018-07-05 22:45:00 +0200

    Check directory existence and skip creation if needed
    Extend HTTParty with PROPFIND method

--- lib/carrierwave/httparty_monkey.rb
+++ lib/carrierwave/httparty_monkey.rb
@@ -8,12 +8,18 @@ module HTTParty
 
   class Request
     SupportedHTTPMethods << Net::HTTP::Mkcol
+    SupportedHTTPMethods << Net::HTTP::Propfind
   end
 
   module ClassMethods
     # Perform a MKCOL request to a path
     def mkcol(path, options = {}, &block)
       perform_request Net::HTTP::Mkcol, path, options, &block
+    end
+
+    # Perform a PROPFIND request to a path
+    def propfind(path, options = {}, &block)
+      perform_request Net::HTTP::Propfind, path, options, &block
     end
   end
 
@@ -24,5 +30,8 @@ module HTTParty
   def self.mkcol(*args, &block)
     Basement.mkcol(*args, &block)
   end
-end # HTTParty
 
+  def self.propfind(*args, &block)
+    Basement.propfind(*args, &block)
+  end
+end # HTTParty
